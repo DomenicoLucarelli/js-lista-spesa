eda2a9eedee059814a67833fc74637b9da4325d1
DomenicoLucarelli <domyluca@gmail.com>
2023-03-01 16:50:34 +0100

    esercizio terminato

---
+++ script.js
@@ -0,0 +1,12 @@
+let list = ['pane', 'pasta', 'latte', 'tovaglioli', 'piatti' ,'forchette'];
+
+let index = 0;
+
+console.log(list)
+
+while(index < list.length ){
+    
+    
+    document.writeln((index + 1) +')' + list[index] + '<br>');
+    index++;
+}
